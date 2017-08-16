dccc70c98baee82e01527bbea07f77db65acfc87
Jordan Last <jordan.michael.last@gmail.com>
2017-08-16 11:03:26 -0600

    use srcdoc

--- secure-eval.ts
+++ secure-eval.ts
@@ -3,7 +3,40 @@ export function secureEval(code: string): Promise<void> {
         const secureEvalIframe: HTMLIFrameElement = document.createElement('iframe');
         secureEvalIframe.setAttribute('sandbox', 'allow-scripts');
         secureEvalIframe.setAttribute('style', 'display: none;');
-        secureEvalIframe.setAttribute('src', '/node_modules/secure-eval/secure-eval-iframe.html'); //TODO is it possible to do this inline? Then I could put the secure eval code in this file and make it even more portable, just a function. It will be possible with the srcdoc attribute, once Edge supports it
+        secureEvalIframe.setAttribute('srcdoc', `
+            <script>
+                const evalWorkerSource = \`
+                    onmessage = function(event) {
+                        try {
+                            eval(event.data);
+                        }
+                        catch(error) {
+                            postMessage({
+                                error: error.toString()
+                            });
+                        }
+                    }
+                \`;
+
+                window.addEventListener('message', (event) => {
+                    const blob = new window.Blob([evalWorkerSource]);
+                    const objectURL = window.URL.createObjectURL(blob);
+                    const evalWorker = new Worker(objectURL);
+
+                    evalWorker.postMessage(event.data);
+
+                    setTimeout(() => { // Terminate the web worker if it runs for too long
+                        evalWorker.terminate();
+                    }, 10000);
+
+                    evalWorker.addEventListener('message', (event) => {
+                        window.parent.postMessage(Object.assign({}, event.data, {
+                            type: 'secure-eval-iframe-result'
+                        }), '*');
+                    });
+                });
+            </script>
+        `);
 
         secureEvalIframe.addEventListener('load', () => {
             secureEvalIframe.contentWindow.postMessage(code, '*'); // It is fine to the the targetOrigin as *, because we do not care if a malicious site reads the code that we send. The code is not expected to be confidential if it is client-side. We only care that when the code is executed that it does not cause harm, which is why it is going to the secure iframe
