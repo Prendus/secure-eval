8d8fad17740cce8f10e65634fa5f85216fc2e607
Jordan Last <jordan.michael.last@gmail.com>
2017-08-16 10:36:09 -0600

    absolute path for iframe for now

--- secure-eval.ts
+++ secure-eval.ts
@@ -3,7 +3,7 @@ export function secureEval(code: string): Promise<void> {
         const secureEvalIframe: HTMLIFrameElement = document.createElement('iframe');
         secureEvalIframe.setAttribute('sandbox', 'allow-scripts');
         secureEvalIframe.setAttribute('style', 'display: none;');
-        secureEvalIframe.setAttribute('src', 'node_modules/secure-eval/secure-eval-iframe.html'); //TODO is it possible to do this inline? Then I could put the secure eval code in this file and make it even more portable, just a function. It will be possible with the srcdoc attribute, once Edge supports it
+        secureEvalIframe.setAttribute('src', '/node_modules/secure-eval/secure-eval-iframe.html'); //TODO is it possible to do this inline? Then I could put the secure eval code in this file and make it even more portable, just a function. It will be possible with the srcdoc attribute, once Edge supports it
 
         secureEvalIframe.addEventListener('load', () => {
             secureEvalIframe.contentWindow.postMessage(code, '*'); // It is fine to the the targetOrigin as *, because we do not care if a malicious site reads the code that we send. The code is not expected to be confidential if it is client-side. We only care that when the code is executed that it does not cause harm, which is why it is going to the secure iframe
